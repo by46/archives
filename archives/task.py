7f242b55beeca0440db5ddf4746e4083e412cacb
benjamin.c.yan <benjamin.c.yan@newegg.com>
2016-08-04 14:41:05 +0800

    init

--- archives/task.py
+++ archives/task.py
@@ -1,8 +1,21 @@
 from __future__ import print_function
+import os.path
+import os
+from git import Repo
 
 from archives import celery
 
 
+def build_workspace(repository):
+    return 'd:\\tmp\\taxi'
+
+
 @celery.task
 def build(branch, username, email, repository):
+    workspace = build_workspace(repository)
+    if os.path.exists(workspace):
+        repo = Repo(workspace)
+    else:
+        repo = Repo.clone_from(repository, 'd:\\tmp\\taxi', branch='master')
+
     print(branch, username, email, repository)
