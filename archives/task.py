9f9162ee6dda10cdd5337a3cc7e85efdba772348
benjamin.c.yan <benjamin.c.yan@newegg.com>
2016-08-04 15:13:27 +0800

    add docs

--- archives/task.py
+++ archives/task.py
@@ -1,13 +1,30 @@
 from __future__ import print_function
-import os.path
+
 import os
+import tempfile
+from urlparse import urlparse
+
 from git import Repo
 
+from archives import app
 from archives import celery
 
 
+def ensure_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
 def build_workspace(repository):
-    return 'd:\\tmp\\taxi'
+    opt = urlparse(repository)
+    parts = opt.path.split('/')
+    group = parts[-2]
+    project_slug = parts[-1]
+    project_slug = project_slug.split('.')[0]
+
+    tmp_dir = os.path.join(tempfile.gettempdir(), app.import_name, group.lower(), project_slug.lower())
+    ensure_dir(tmp_dir)
+    return os.path.join(tmp_dir, 'git')
 
 
 @celery.task
@@ -18,6 +35,6 @@ def build(branch, username, email, repository):
         origin = repo.remotes['origin']
         origin.pull()
     else:
-        Repo.clone_from(repository, 'd:\\tmp\\taxi', branch='master')
+        Repo.clone_from(repository, workspace, branch='master')
 
     print(branch, username, email, repository)
---
+++ docs/api.rst
@@ -0,0 +1,15 @@
+API
+=====
+
+objson
+---------
+
+.. automodule:: objson
+    :members:
+    :undoc-members:
+
+config
+---------
+
+.. automodule:: config
+    :members:
