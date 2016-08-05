1b92fea324e65a5c4f51d7a88edff9f1c8aef96f
benjamin.c.yan <benjamin.c.yan@newegg.com>
2016-08-05 13:38:52 +0800

    format end line

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,4 +2,4 @@
 
 IMAGE=$(sirius docker_image_name | head -n 1)
 
-sirius docker_deploy:deimos,${IMAGE},server=scdfis01,ports="9200;8080"
+sirius docker_deploy:archives,${IMAGE},server=scmesos06,ports="8080;8080"
