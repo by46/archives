e514add222061e60e0b62bd1f21684577bcc24ad
benjamin.c.yan <benjamin.c.yan@newegg.com>
2016-08-05 16:08:53 +0800

    debug

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,4 +2,4 @@
 
 IMAGE=$(sirius docker_image_name | head -n 1)
 
-sirius docker_deploy:archives,${IMAGE},server=scmesos06,ports="8080;8080", env="ENV\=prd"
+sirius docker_deploy:archives,${IMAGE},server=scmesos06,ports="8080;8080",env="ENV\=prd"
