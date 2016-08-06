71a161736103d9188f1db1b4d4a8a9e71d2fdce3
benjamin.c.yan <benjamin.c.yan@newegg.com>
2016-08-06 16:20:28 +0800

    fix deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,4 +2,4 @@
 
 IMAGE=$(sirius docker_image_name | head -n 1)
 
-sirius docker_deploy:archives,${IMAGE},server=scmesos06,ports="8080;8080",volumes="/opt/archives;/opt/data",env="ENV\=prd"
+sirius docker_deploy:archives,${IMAGE},server=scmesos06,ports="8080;8080",volumes="/opt/archives;/opt/data",env="ENV\=prd;C_FORCE_ROOT=1"
